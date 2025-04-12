80b4757a70809290fc3116133ddc69c037f242b2
Twingodstudio <154692221+Twingodstudio@users.noreply.github.com>
2025-04-12 06:22:41 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'Bot',
   botName: "Kenny the Bot",
   botOwner: 'twingod', // Your name
-  ownerNumber: '23480g51227885', //Your number
+  ownerNumber: '2348051227885', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
